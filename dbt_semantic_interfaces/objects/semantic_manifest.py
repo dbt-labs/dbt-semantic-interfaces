169c55ba0e9d5faa84d46a05a21f638f1c8eb7f4
Callum McCann <cmccann51@gmail.com>
2023-05-17 14:56:53 -0500

    Make `interfaces_version` a direct attribute
    `interfaces_version` has to be a direct attribute of the `SemanticManifest`
    for deserialization to work properly. With it being a direct attribute, we
    had to begin using the pydantic validator to ensure the version gets set
    on `SemanticManifest` instantiation.
    Note: Callum McCann authored this commit, but I (Quigley Malcolm) have updated
    the commit message.

--- dbt_semantic_interfaces/objects/semantic_manifest.py
+++ dbt_semantic_interfaces/objects/semantic_manifest.py
@@ -1,6 +1,7 @@
 from typing import List
 
-from importlib_metadata import PackageNotFoundError, version
+from importlib_metadata import version
+from pydantic import validator
 
 from dbt_semantic_interfaces.objects.base import HashableBaseModel
 from dbt_semantic_interfaces.objects.metric import Metric
@@ -12,11 +13,12 @@ class SemanticManifest(HashableBaseModel):
 
     semantic_models: List[SemanticModel]
     metrics: List[Metric]
+    interfaces_version: str = ""
 
-    @property
-    def interfaces_version(self) -> str:
+    @validator("interfaces_version", always=True)
+    @classmethod
+    def __create_default_interfaces_version(cls, value: str) -> str:  # type: ignore[misc]
         """Returns the version of the dbt_semantic_interfaces package that generated this manifest."""
-        try:
-            return version("dbt_semantic_interfaces")
-        except PackageNotFoundError:
-            return "dbt_semantic_interfaces is not installed"
+        if value:
+            return value
+        return version("dbt_semantic_interfaces")
