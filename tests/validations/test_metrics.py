8c240e78a6b2f099fd73c12dd3a1bc1435dbcc95
Paul Yang <github@ardorful.com>
2023-06-15 15:58:26 -0700

    Update tests to reflect metric schema changes.

--- tests/validations/test_metrics.py
+++ tests/validations/test_metrics.py
@@ -78,7 +78,7 @@ def test_metric_no_time_dim_dim_only_source() -> None:  # noqa:D
                 metric_with_guaranteed_meta(
                     name="metric_with_no_time_dim",
                     type=MetricType.SIMPLE,
-                    type_params=PydanticMetricTypeParams(measures=[PydanticMetricInputMeasure(name=measure_name)]),
+                    type_params=PydanticMetricTypeParams(measure=PydanticMetricInputMeasure(name=measure_name)),
                 )
             ],
         )
@@ -108,7 +108,7 @@ def test_metric_no_time_dim() -> None:  # noqa:D
                     metric_with_guaranteed_meta(
                         name="metric_with_no_time_dim",
                         type=MetricType.SIMPLE,
-                        type_params=PydanticMetricTypeParams(measures=[PydanticMetricInputMeasure(name=measure_name)]),
+                        type_params=PydanticMetricTypeParams(measure=PydanticMetricInputMeasure(name=measure_name)),
                     )
                 ],
             )
@@ -149,7 +149,7 @@ def test_metric_multiple_primary_time_dims() -> None:  # noqa:D
                     metric_with_guaranteed_meta(
                         name="foo",
                         type=MetricType.SIMPLE,
-                        type_params=PydanticMetricTypeParams(measures=[PydanticMetricInputMeasure(name=measure_name)]),
+                        type_params=PydanticMetricTypeParams(measure=PydanticMetricInputMeasure(name=measure_name)),
                     )
                 ],
             )
@@ -221,12 +221,12 @@ def test_derived_metric() -> None:  # noqa: D
                 metric_with_guaranteed_meta(
                     name="random_metric",
                     type=MetricType.SIMPLE,
-                    type_params=PydanticMetricTypeParams(measures=[PydanticMetricInputMeasure(name=measure_name)]),
+                    type_params=PydanticMetricTypeParams(measure=PydanticMetricInputMeasure(name=measure_name)),
                 ),
                 metric_with_guaranteed_meta(
                     name="random_metric2",
                     type=MetricType.SIMPLE,
-                    type_params=PydanticMetricTypeParams(measures=[PydanticMetricInputMeasure(name=measure_name)]),
+                    type_params=PydanticMetricTypeParams(measure=PydanticMetricInputMeasure(name=measure_name)),
                 ),
                 metric_with_guaranteed_meta(
                     name="alias_collision",
