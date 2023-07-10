03e2bf20713298e53d4e58a92d38ae6d4d5f54dc
Quigley Malcolm <quigley.malcolm@dbtlabs.com>
2023-07-10 10:41:47 -0700

    Rework old call parameter tests to work off of where filter compiler

--- tests/jinja_compilers/test_where_filter_compiler.py
+++ tests/jinja_compilers/test_where_filter_compiler.py
@@ -1,79 +1,66 @@
 import logging
 
-from dbt_semantic_interfaces.implementations.filters.call_parameter_sets import (
-    DimensionCallParameterSet,
-    EntityCallParameterSet,
-    FilterCallParameterSets,
-    TimeDimensionCallParameterSet,
-)
 from dbt_semantic_interfaces.implementations.filters.where_filter import (
+    PydanticDimensionInput,
+    PydanticEntityInput,
+    PydanticTimeDimensionInput,
     PydanticWhereFilter,
 )
-from dbt_semantic_interfaces.references import (
-    DimensionReference,
-    EntityReference,
-    TimeDimensionReference,
-)
 from dbt_semantic_interfaces.type_enums import TimeGranularity
+from tests.fixtures.semantic_manifest_fixtures import PydanticWhereFilterCompilerT
 
 logger = logging.getLogger(__name__)
 
 
-def test_extract_dimension_call_parameter_sets() -> None:  # noqa: D
-    parse_result = PydanticWhereFilter(
+def test_extract_dimension_inputs(  # noqa: D
+    pydantic_where_filter_compiler: PydanticWhereFilterCompilerT,
+) -> None:
+    compilation_result = pydantic_where_filter_compiler.compile(
         where_sql_template=(
             """{{ dimension('is_instant') }} AND {{ dimension('country', entity_path=['listing']) }} == 'US'"""
         )
-    ).call_parameter_sets
+    )
 
-    assert parse_result == FilterCallParameterSets(
-        dimension_call_parameter_sets=(
-            DimensionCallParameterSet(
-                dimension_reference=DimensionReference(element_name="is_instant"),
-                entity_path=(),
-            ),
-            DimensionCallParameterSet(
-                entity_path=(EntityReference(element_name="listing"),),
-                dimension_reference=DimensionReference(element_name="country"),
-            ),
-        ),
-        entity_call_parameter_sets=(),
+    assert compilation_result == PydanticWhereFilter(
+        where_sql_template="""{{ is_instant }} AND {{ country }} == 'US'""",
+        input_dimensions=[
+            PydanticDimensionInput(name="is_instant"),
+            PydanticDimensionInput(name="country", entity_path=["listing"]),
+        ],
+        input_time_dimensions=[],
+        input_entities=[],
     )
 
 
-def test_extract_time_dimension_call_parameter_sets() -> None:  # noqa: D
-    parse_result = PydanticWhereFilter(
+def test_extract_time_dimension_inputs(  # noqa: D
+    pydantic_where_filter_compiler: PydanticWhereFilterCompilerT,
+) -> None:
+    compilation_result = pydantic_where_filter_compiler.compile(
         where_sql_template="""{{ time_dimension('created_at', 'month', entity_path=['listing']) }} = '2020-01-01'"""
-    ).call_parameter_sets
+    )
 
-    assert parse_result == FilterCallParameterSets(
-        time_dimension_call_parameter_sets=(
-            TimeDimensionCallParameterSet(
-                time_dimension_reference=TimeDimensionReference(element_name="created_at"),
-                entity_path=(EntityReference(element_name="listing"),),
-                time_granularity=TimeGranularity.MONTH,
-            ),
-        )
+    assert compilation_result == PydanticWhereFilter(
+        where_sql_template="""{{ created_at }} = '2020-01-01'""",
+        input_time_dimensions=[
+            PydanticTimeDimensionInput(name="created_at", granularity=TimeGranularity.MONTH, entity_path=["listing"])
+        ],
+        input_dimensions=[],
+        input_entities=[],
     )
 
 
-def test_extract_entity_call_parameter_sets() -> None:  # noqa: D
-    parse_result = PydanticWhereFilter(
+def test_extract_entity_inputs(  # noqa: D
+    pydantic_where_filter_compiler: PydanticWhereFilterCompilerT,
+) -> None:
+    compilation_result = pydantic_where_filter_compiler.compile(
         where_sql_template=(
             """{{ entity('listing') }} AND {{ entity('user', entity_path=['listing']) }} == 'TEST_USER_ID'"""
         )
-    ).call_parameter_sets
+    )
 
-    assert parse_result == FilterCallParameterSets(
-        dimension_call_parameter_sets=(),
-        entity_call_parameter_sets=(
-            EntityCallParameterSet(
-                entity_path=(),
-                entity_reference=EntityReference("listing"),
-            ),
-            EntityCallParameterSet(
-                entity_path=(EntityReference("listing"),),
-                entity_reference=EntityReference("user"),
-            ),
-        ),
+    assert compilation_result == PydanticWhereFilter(
+        where_sql_template="""{{ listing }} AND {{ user }} == 'TEST_USER_ID'""",
+        input_entities=[PydanticEntityInput(name="listing"), PydanticEntityInput(name="user", entity_path=["listing"])],
+        input_dimensions=[],
+        input_time_dimensions=[],
     )
