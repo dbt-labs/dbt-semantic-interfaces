0d4b70fcbea993855e9daefc89ffc6ccc9c3d04b
Paul Yang <github@ardorful.com>
2023-09-13 13:17:47 -0700

    Add saved query protocol and implementation.

---
+++ dbt_semantic_interfaces/implementations/saved_query.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from typing import List, Optional
+
+from typing_extensions import override
+
+from dbt_semantic_interfaces.implementations.base import (
+    HashableBaseModel,
+    ModelWithMetadataParsing,
+)
+from dbt_semantic_interfaces.implementations.filters.where_filter import (
+    PydanticWhereFilter,
+)
+from dbt_semantic_interfaces.implementations.metadata import PydanticMetadata
+from dbt_semantic_interfaces.protocols import ProtocolHint
+from dbt_semantic_interfaces.protocols.saved_query import SavedQuery
+
+
+class PydanticSavedQuery(HashableBaseModel, ModelWithMetadataParsing, ProtocolHint[SavedQuery]):
+    """Pydantic implementation of SavedQuery."""
+
+    @override
+    def _implements_protocol(self) -> SavedQuery:
+        return self
+
+    name: str
+    metrics: List[str]
+    group_bys: List[str] = []
+    where: List[PydanticWhereFilter] = []
+
+    description: Optional[str] = None
+    metadata: Optional[PydanticMetadata] = None
--- dbt_semantic_interfaces/implementations/semantic_manifest.py
+++ dbt_semantic_interfaces/implementations/semantic_manifest.py
@@ -7,6 +7,7 @@ from dbt_semantic_interfaces.implementations.metric import PydanticMetric
 from dbt_semantic_interfaces.implementations.project_configuration import (
     PydanticProjectConfiguration,
 )
+from dbt_semantic_interfaces.implementations.saved_query import PydanticSavedQuery
 from dbt_semantic_interfaces.implementations.semantic_model import PydanticSemanticModel
 from dbt_semantic_interfaces.protocols import ProtocolHint, SemanticManifest
 
@@ -21,3 +22,4 @@ class PydanticSemanticManifest(HashableBaseModel, ProtocolHint[SemanticManifest]
     semantic_models: List[PydanticSemanticModel]
     metrics: List[PydanticMetric]
     project_configuration: PydanticProjectConfiguration
+    saved_queries: List[PydanticSavedQuery] = []
