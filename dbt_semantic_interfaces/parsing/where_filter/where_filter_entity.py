7a948b9f8383d7e7cd899f4147497f0089f5162a
Courtney Holcomb <courtneyeholcomb@gmail.com>
2024-03-20 19:53:47 -0700

    Allow metrics in filters (#274) (#275)
    ### Description
    Backporting https://github.com/dbt-labs/dbt-semantic-interfaces/pull/274
    into `0.5.latest`.

--- dbt_semantic_interfaces/parsing/where_filter/where_filter_entity.py
+++ dbt_semantic_interfaces/parsing/where_filter/where_filter_entity.py
@@ -4,7 +4,11 @@ from typing import List, Sequence
 
 from typing_extensions import override
 
-from dbt_semantic_interfaces.call_parameter_sets import EntityCallParameterSet
+from dbt_semantic_interfaces.call_parameter_sets import (
+    EntityCallParameterSet,
+    MetricCallParameterSet,
+)
+from dbt_semantic_interfaces.errors import InvalidQuerySyntax
 from dbt_semantic_interfaces.parsing.where_filter.parameter_set_factory import (
     ParameterSetFactory,
 )
@@ -12,6 +16,8 @@ from dbt_semantic_interfaces.protocols.protocol_hint import ProtocolHint
 from dbt_semantic_interfaces.protocols.query_interface import (
     QueryInterfaceEntity,
     QueryInterfaceEntityFactory,
+    QueryInterfaceMetric,
+    QueryInterfaceMetricFactory,
 )
 
 
@@ -25,6 +31,20 @@ class EntityStub(ProtocolHint[QueryInterfaceEntity]):
     @override
     def _implements_protocol(self) -> QueryInterfaceEntity:
         return self
+
+
+class MetricStub(ProtocolHint[QueryInterfaceMetric]):
+    """A Metric implementation that just satisfies the protocol.
+
+    QueryInterfaceMetric currently has no methods and the parameter set is created in the factory.
+    """
+
+    @override
+    def _implements_protocol(self) -> QueryInterfaceMetric:
+        return self
+
+    def descending(self, _is_descending: bool) -> QueryInterfaceMetric:  # noqa: D
+        raise InvalidQuerySyntax("descending is invalid in the where parameter and filter spec")
 
 
 class WhereFilterEntityFactory(ProtocolHint[QueryInterfaceEntityFactory]):
@@ -41,3 +61,20 @@ class WhereFilterEntityFactory(ProtocolHint[QueryInterfaceEntityFactory]):
         """Gets called by Jinja when rendering {{ Entity(...) }}."""
         self.entity_call_parameter_sets.append(ParameterSetFactory.create_entity(entity_name, entity_path))
         return EntityStub()
+
+
+class WhereFilterMetricFactory(ProtocolHint[QueryInterfaceMetricFactory]):
+    """Executes in the Jinja sandbox to produce parameter sets and append them to a list."""
+
+    @override
+    def _implements_protocol(self) -> QueryInterfaceMetricFactory:
+        return self
+
+    def __init__(self) -> None:  # noqa: D
+        self.metric_call_parameter_sets: List[MetricCallParameterSet] = []
+
+    def create(self, metric_name: str, group_by: Sequence[str] = ()) -> MetricStub:  # noqa: D
+        self.metric_call_parameter_sets.append(
+            ParameterSetFactory.create_metric(metric_name=metric_name, group_by=group_by)
+        )
+        return MetricStub()
