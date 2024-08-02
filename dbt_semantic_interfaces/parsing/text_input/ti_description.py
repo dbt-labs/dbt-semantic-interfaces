3aaeca3352aace858ddbd72c4120c7c6b33c63d1
Paul Yang <github@ardorful.com>
2024-08-02 10:29:01 -0700

    Streamline object-builder text parsing (#322)
    ### Description
    
    When parsing the Jinja template in where-filters, there is currently a
    set of factory and factory-specific classes to implement a specific
    behavior. I've noticed that there are multiple implementations of those
    classes (if you add the ones in this repo and dependent repos), and it
    seemed like there was an opportunity to streamline the structure to
    handle the update for saved-queries and other changes down the road.
    
    This PR adds a `QueryItemDescription` that encapsulates what the user
    has specified in the text input, and also adds a
    `QueryItemTextProcessor` that allows you to:
    
    * Find all the `QueryItemDescription`s in a template.
    * Render a template for a given `QueryItemDescription` using a
    configurable format.
    * Configure what builder methods (e.g. `.descending()`) are allowed in
    which cases.
    
    These features seem like it would support existing use cases, but
    tagging @DevonFulcher to confirm.
    
    Also, please view by commit. The first commit includes the interface, so
    if there are issues there, it would be better to get those resolved
    before updating the later commits.
    
    ### Checklist
    
    - [x] I have read [the contributing
    guide](https://github.com/dbt-labs/dbt-semantic-interfaces/blob/main/CONTRIBUTING.md)
    and understand what's expected of me
    - [x] I have signed the
    [CLA](https://docs.getdbt.com/docs/contributor-license-agreements)
    - [x] This PR includes tests, or tests are not required/relevant for
    this PR
    - [x] I have run `changie new` to [create a changelog
    entry](https://github.com/dbt-labs/dbt-semantic-interfaces/blob/main/CONTRIBUTING.md#adding-a-changelog-entry)

---
+++ dbt_semantic_interfaces/parsing/text_input/ti_description.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional, Tuple
+
+
+@dataclass(frozen=True)
+class QueryItemDescription:
+    """Describes a query item specified by the user.
+
+    For example, the following specified in an order-by of a saved query:
+
+        Dimension("user__created_at", entity_path=['listing']).grain('day').date_part('month').descending(True)
+
+        ->
+
+        QueryItemDescription(
+            item_type=GroupByItemType.DIMENSION,
+            item_name="user__created_at",
+            entity_path=['listing'],
+            time_granularity_name='day'
+            date_part_name='month'
+            descending=True
+        )
+
+    * This is named "...Description" to keep it general as the way users specify query items will change significantly
+      in the not-so-distant future.
+    * This can be later expanded to a set of classes for better typing.
+    """
+
+    item_type: QueryItemType
+    item_name: str
+    entity_path: Tuple[str, ...]
+    group_by_for_metric_item: Tuple[str, ...]
+    time_granularity_name: Optional[str]
+    date_part_name: Optional[str]
+    descending: Optional[bool]
+
+    def __post_init__(self) -> None:  # noqa: D105
+        if self.item_type is QueryItemType.ENTITY or self.item_type is QueryItemType.METRIC:
+            assert (
+                self.time_granularity_name is None
+            ), f"{self.time_granularity_name=} is not supported for {self.item_type=}"
+            assert self.date_part_name is None, f"{self.date_part_name=} is not supported for {self.item_type=}"
+
+        if self.item_type is not QueryItemType.METRIC:
+            assert (
+                not self.group_by_for_metric_item
+            ), f"{self.group_by_for_metric_item=} is not supported for {self.item_type=}"
+
+    def create_modified(
+        self,
+        time_granularity_name: Optional[str] = None,
+        date_part_name: Optional[str] = None,
+        descending: Optional[bool] = None,
+    ) -> QueryItemDescription:
+        """Create one with the same fields as self except the ones provided."""
+        return QueryItemDescription(
+            item_type=self.item_type,
+            item_name=self.item_name,
+            entity_path=self.entity_path,
+            time_granularity_name=time_granularity_name or self.time_granularity_name,
+            date_part_name=date_part_name or self.date_part_name,
+            group_by_for_metric_item=self.group_by_for_metric_item,
+            descending=descending or self.descending,
+        )
+
+
+class QueryItemType(Enum):
+    """Enumerates the types of items that a used to group items in a filter or a query.
+
+    e.g. in the object-builder syntax: QueryItemType.DIMENSION refers to `Dimension(...)`.
+
+    The value of the enum is the name of the builder "object".
+    """
+
+    DIMENSION = "Dimension"
+    TIME_DIMENSION = "TimeDimension"
+    ENTITY = "Entity"
+    METRIC = "Metric"
+
+
+class ObjectBuilderMethod(Enum):
+    """In the object builder notation, the possible methods that can be called on the builder object.
+
+    e.g. ObjectBuilderMethod.GRAIN refers to `.grain` in `Dimension(...).grain('month')`
+
+    The value of the enum is the name of the method.
+    """
+
+    GRAIN = "grain"
+    DATE_PART = "date_part"
+    DESCENDING = "descending"
---
+++ dbt_semantic_interfaces/parsing/text_input/ti_processor.py
@@ -0,0 +1,160 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from textwrap import indent
+from typing import List, Sequence
+
+from jinja2 import StrictUndefined, TemplateSyntaxError, UndefinedError
+from jinja2.exceptions import SecurityError
+from jinja2.sandbox import SandboxedEnvironment
+from typing_extensions import override
+
+from dbt_semantic_interfaces.parsing.text_input.description_renderer import (
+    QueryItemDescriptionRenderer,
+)
+from dbt_semantic_interfaces.parsing.text_input.rendering_helper import (
+    ObjectBuilderJinjaRenderHelper,
+)
+from dbt_semantic_interfaces.parsing.text_input.ti_description import (
+    QueryItemDescription,
+)
+from dbt_semantic_interfaces.parsing.text_input.ti_exceptions import (
+    QueryItemJinjaException,
+)
+from dbt_semantic_interfaces.parsing.text_input.valid_method import ValidMethodMapping
+
+
+class QueryItemTextProcessor:
+    """Performs processing actions for text containing query items specified in the object-builder syntax.
+
+    This currently supports:
+    * Collecting `QueryItemDescription`s from a Jinja template.
+    * Rendering a Jinja template using a specified renderer.
+    """
+
+    def collect_descriptions_from_template(
+        self,
+        jinja_template: str,
+        valid_method_mapping: ValidMethodMapping,
+    ) -> Sequence[QueryItemDescription]:
+        """Returns the `QueryItemDescription`s that are found in a Jinja template.
+
+        Args:
+            jinja_template: A Jinja-template string like `{{ Dimension('listing__country') }} = 'US'`.
+            valid_method_mapping: Mapping from the builder object to the valid methods. See
+            `ConfiguredValidMethodMapping`.
+
+        Returns:
+            A sequence of the descriptions found in the template.
+
+        Raises:
+            QueryItemJinjaException: See definition.
+            InvalidBuilderMethodException: See definition.
+        """
+        description_collector = _CollectDescriptionProcessor()
+        self._process_template(
+            jinja_template=jinja_template,
+            valid_method_mapping=valid_method_mapping,
+            description_processor=description_collector,
+        )
+        return description_collector.collected_descriptions()
+
+    def render_template(
+        self,
+        jinja_template: str,
+        renderer: QueryItemDescriptionRenderer,
+        valid_method_mapping: ValidMethodMapping,
+    ) -> str:
+        """Renders the Jinja template using the specified renderer.
+
+        Args:
+            jinja_template: A Jinja template string like `{{ Dimension('listing__country') }} = 'US'`.
+            renderer: The renderer to use for rendering each item.
+            valid_method_mapping: Mapping from the builder object to the valid methods. See
+            `ConfiguredValidMethodMapping`.
+
+        Returns:
+            The rendered Jinja template.
+
+        Raises:
+            QueryItemJinjaException: See definition.
+            InvalidBuilderMethodException: See definition.
+        """
+        render_processor = _RendererProcessor(renderer)
+        return self._process_template(
+            jinja_template=jinja_template,
+            valid_method_mapping=valid_method_mapping,
+            description_processor=render_processor,
+        )
+
+    def _process_template(
+        self,
+        jinja_template: str,
+        valid_method_mapping: ValidMethodMapping,
+        description_processor: QueryItemDescriptionProcessor,
+    ) -> str:
+        """Helper to run a `QueryItemDescriptionProcessor` on a Jinja template."""
+        render_helper = ObjectBuilderJinjaRenderHelper(
+            description_processor=description_processor,
+            valid_method_mapping=valid_method_mapping,
+        )
+        try:
+            # the string that the sandbox renders is unused
+            rendered = (
+                SandboxedEnvironment(undefined=StrictUndefined)
+                .from_string(jinja_template)
+                .render(
+                    Dimension=render_helper.get_function_for_dimension(),
+                    TimeDimension=render_helper.get_function_for_time_dimension(),
+                    Entity=render_helper.get_function_for_entity(),
+                    Metric=render_helper.get_function_for_metric(),
+                )
+            )
+        except (UndefinedError, TemplateSyntaxError, SecurityError) as e:
+            raise QueryItemJinjaException(
+                f"Error while processing Jinja template:" f"\n{indent(jinja_template, prefix='    ')}"
+            ) from e
+
+        return rendered
+
+
+class QueryItemDescriptionProcessor(ABC):
+    """General processor that does something to a query-item description seen in a Jinja template."""
+
+    @abstractmethod
+    def process_description(self, item_description: QueryItemDescription) -> str:
+        """Process the given description, and return a string that would be substituted into the Jinja template."""
+        raise NotImplementedError
+
+
+class _CollectDescriptionProcessor(QueryItemDescriptionProcessor):
+    """Processor that collects all descriptions that were processed."""
+
+    def __init__(self) -> None:  # noqa: D107
+        self._items: List[QueryItemDescription] = []
+
+    def collected_descriptions(self) -> Sequence[QueryItemDescription]:
+        """Return all descriptions that were processed so far."""
+        return self._items
+
+    @override
+    def process_description(self, item_description: QueryItemDescription) -> str:
+        if item_description not in self._items:
+            self._items.append(item_description)
+
+        return ""
+
+
+class _RendererProcessor(QueryItemDescriptionProcessor):
+    """Processor that renders the descriptions in a Jinja template using the given renderer.
+
+    This is just a pass-through, but it allows `QueryItemDescriptionRenderer` to be a facade that has more appropriate
+    method names.
+    """
+
+    def __init__(self, renderer: QueryItemDescriptionRenderer) -> None:  # noqa: D107
+        self._renderer = renderer
+
+    @override
+    def process_description(self, item_description: QueryItemDescription) -> str:
+        return self._renderer.render_description(item_description)
