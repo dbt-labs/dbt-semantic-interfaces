b5feed66f9693a9555830eb169cad39538bbe9e2
Devon Fulcher <24593113+DevonFulcher@users.noreply.github.com>
2024-10-15 11:52:11 -0500

    Downgrade pydantic in dev env (#355)
    ### Description
    
    This PR makes type-checking more consistent for local development. I
    have had an annoying issue with the type-checker reporting errors that
    weren't appearing in CI. Initially, I thought this was a problem with
    mypy, but actually the problem was that I have been using Pydantic 2,
    but the types in the codebase are only compatible with Pydantic 1.
    Specifically, I was getting a lot of errors for properties like
    `extra_detail` that don't have a default value:
    
    ```
    class ValidationIssue(ABC, BaseModel):
        """The abstract base ValidationIssue class that the specific ValidationIssue classes are built from."""
    
        message: str
        context: Optional[ValidationContext] = None
        extra_detail: Optional[str]
    ```
    
    ### Checklist
    
    - [x] I have read [the contributing
    guide](https://github.com/dbt-labs/dbt-semantic-interfaces/blob/main/CONTRIBUTING.md)
    and understand what's expected of me
    - [x] I have signed the
    [CLA](https://docs.getdbt.com/docs/contributor-license-agreements)
    - [x] This PR includes tests, or tests are not required/relevant for
    this PR
    - [x] I have run `changie new` to [create a changelog
    entry](https://github.com/dbt-labs/dbt-semantic-interfaces/blob/main/CONTRIBUTING.md#adding-a-changelog-entry)
    
    ---------
    
    Co-authored-by: Courtney Holcomb <courtneyeholcomb@gmail.com>

--- dsi_pydantic_shim.py
+++ dsi_pydantic_shim.py
@@ -1,3 +1,13 @@
+"""Shim to allow support for both Pydantic 1 and Pydantic 2.
+
+DSI must support both major versions of Pydantic because dbt-core depends on DSI. dbt-core users might be using an
+environment with either version, and we can't restrict them to one or the other. Here, we essentially import all
+Pydantic objects from version 1. Throughout the repo, we import those objects from this file instead of from Pydantic
+directly, meaning that we essentially only use Pydantic 1 in this repo, but without forcing that restriction on dbt
+users. The development environment for this repo should be pinned to Pydantic 1 to ensure devs get appropriate type
+hints.
+"""
+
 from importlib.metadata import version
 
 pydantic_version = version("pydantic")
