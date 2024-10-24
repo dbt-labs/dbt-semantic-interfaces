ada4b16e8c90cc05427efa5afce7a5198a36ff3b
Devon Fulcher <24593113+DevonFulcher@users.noreply.github.com>
2024-10-24 14:07:22 -0500

    Rename to PydanticSemanticLayerElementConfig

--- dbt_semantic_interfaces/protocols/dimension.py
+++ dbt_semantic_interfaces/protocols/dimension.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from abc import abstractmethod
 from typing import Optional, Protocol
 
-from dbt_semantic_interfaces.protocols.meta import ConfigMeta
+from dbt_semantic_interfaces.protocols.meta import PydanticSemanticLayerElementConfig
 from dbt_semantic_interfaces.protocols.metadata import Metadata
 from dbt_semantic_interfaces.references import (
     DimensionReference,
@@ -111,5 +111,5 @@ class Dimension(Protocol):
 
     @property
     @abstractmethod
-    def config(self) -> Optional[ConfigMeta]:  # noqa: D
+    def config(self) -> Optional[PydanticSemanticLayerElementConfig]:  # noqa: D
         pass
