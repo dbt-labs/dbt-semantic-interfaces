7c59d74583a50ea28463af3a78afbf6023cfab89
Will Deng <william.deng14@gmail.com>
2024-11-08 13:37:40 -0500

    drop default

--- dbt_semantic_interfaces/parsing/where_filter/parameter_set_factory.py
+++ dbt_semantic_interfaces/parsing/where_filter/parameter_set_factory.py
@@ -46,10 +46,10 @@ class ParameterSetFactory:
     @staticmethod
     def create_time_dimension(
         time_dimension_name: str,
+        custom_granularity_names: Sequence[str],
         time_granularity_name: Optional[str] = None,
         entity_path: Sequence[str] = (),
         date_part_name: Optional[str] = None,
-        custom_granularity_names: Sequence[str] = (),
     ) -> TimeDimensionCallParameterSet:
         """Gets called by Jinja when rendering {{ TimeDimension(...) }}.
 
